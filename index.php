6aee31e6a060f2070714353eefa7bf490c023626
isredaphim <55270578+isredaphim@users.noreply.github.com>
2020-11-06 16:35:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php
 <?xml version="1.0" encoding="utf-8"?>
 
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
@@ -23,3 +24,4 @@
      
  </g>
 </svg>
+?>
