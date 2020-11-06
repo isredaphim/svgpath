192ef8afc06a43a4c92e3dfb6327386f6e827dcf
isredaphim <55270578+isredaphim@users.noreply.github.com>
2020-11-06 16:47:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-
 <?xml version="1.0" encoding="utf-8"?>
-<g>
-<g>
-<title>SVG PATHOLOGY</title>
-</g>
+
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 1366 768" style="enable-background:new 0 0 1366 768;" xml:space="preserve">
 <style type="text/css">
     .st0{fill:transparent;}
+    
 </style>
 <g id="layer1" transform="translate(-362.5464,-406.21469)">
 	<path id="path10000" class="st0" d="M974.86,592.21c-53.25,0-106.18,23.82-143.84,64.74c-23.73,25.78-41.19,58.14-50.87,93.02
@@ -16,7 +13,7 @@
 		s-8.12-42.72-22.06-57.87c-13.86-15.06-33.31-23.85-52.91-23.96c-3.82-38.83-20.01-76.2-45.45-103.83
 		c-29.29-31.82-70.45-50.35-111.87-50.35c-10.06,0-20.09,1.12-29.95,3.23c-7.22-11.7-15.43-22.66-24.6-32.62
 		C1081.04,616.04,1028.11,592.21,974.86,592.21L974.86,592.21z"/>
-	</g>
+
   <text y="40" font-size="36" fill="black">
     <textPath href="#path10000">
       "Clouds come floating into my life, no longer to carry rain or usher storm, but to add color to my sunset sky -Rabindrath Tagore"
@@ -26,4 +23,3 @@
      
  </g>
 </svg>
-
